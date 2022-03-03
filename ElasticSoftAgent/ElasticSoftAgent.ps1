3048d0b272f0e596e0dd841a72c30cb9c077d34e
Sénéchal Julien <35048771+Teckinfor@users.noreply.github.com>
2022-03-03 12:00:37 +0000

    Update ElasticSoftAgent.ps1

--- ElasticSoftAgent/ElasticSoftAgent.ps1
+++ ElasticSoftAgent/ElasticSoftAgent.ps1
@@ -1,134 +1,71 @@
-﻿function Get-Software
+function Get-Software
 {
-    <#
-        .SYNOPSIS
-        Reads installed software from registry
-
-        .PARAMETER DisplayName
-        Name or part of name of the software you are looking for
-
-        .EXAMPLE
-        Get-Software -DisplayName *Office*
-        returns all software with "Office" anywhere in its name
-    #>
-
     param
     (
-    # emit only software that matches the value you submit:
     [string]
     $DisplayName = '*'
     )
 
+        $Values = 'DisplayName','DisplayVersion'
 
-    #region define friendly texts:
-    $Scopes = @{
-        HKLM = 'All Users'
-        HKCU = 'Current User'
-    }
 
-    $Architectures = @{
-        $true = '32-Bit'
-        $false = '64-Bit'
-    }
-    #endregion
-
-    #region define calculated custom properties:
-        # add the scope of the software based on whether the key is located
-        # in HKLM: or HKCU:
-        $Scope = @{
-            Name = 'Scope'
-            Expression = {
-            $Scopes[$_.PSDrive.Name]
-            }
-        }
-
-        # add architecture (32- or 64-bit) based on whether the registry key 
-        # contains the parent key WOW6432Node:
-        $Architecture = @{
-        Name = 'Architecture'
-        Expression = {$Architectures[$_.PSParentPath -like '*\WOW6432Node\*']}
-        }
-    #endregion
-
-    #region define the properties (registry values) we are after
-        # define the registry values that you want to include into the result:
-        $Values = 'AuthorizedCDFPrefix',
-                    'Comments',
-                    'Contact',
-                    'DisplayName',
-                    'DisplayVersion',
-                    'EstimatedSize',
-                    'HelpLink',
-                    'HelpTelephone',
-                    'InstallDate',
-                    'InstallLocation',
-                    'InstallSource',
-                    'Language',
-                    'ModifyPath',
-                    'NoModify',
-                    'PSChildName',
-                    'PSDrive',
-                    'PSParentPath',
-                    'PSPath',
-                    'PSProvider',
-                    'Publisher',
-                    'Readme',
-                    'Size',
-                    'SystemComponent',
-                    'UninstallString',
-                    'URLInfoAbout',
-                    'URLUpdateInfo',
-                    'Version',
-                    'VersionMajor',
-                    'VersionMinor',
-                    'WindowsInstaller',
-                    'Scope',
-                    'Architecture'
-    #endregion
-
-    #region Define the VISIBLE properties
-        # define the properties that should be visible by default
-        # keep this below 5 to produce table output:
-        [string[]]$visible = 'DisplayName','DisplayVersion','Scope','Architecture'
+        [string[]]$visible = 'DisplayName','DisplayVersion'
         [Management.Automation.PSMemberInfo[]]$visibleProperties = [System.Management.Automation.PSPropertySet]::new('DefaultDisplayPropertySet',$visible)
-    #endregion
 
-    #region read software from all four keys in Windows Registry:
-        # read all four locations where software can be registered, and ignore non-existing keys:
-        Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                            'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                            'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                            'HKCU:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction Ignore |
-        # exclude items with no DisplayName:
-        Where-Object DisplayName |
-        # include only items that match the user filter:
-        Where-Object { $_.DisplayName -like $DisplayName } |
-        # add the two calculated properties defined earlier:
-        Select-Object -Property *, $Scope, $Architecture |
-        # create final objects with all properties we want:
-        Select-Object -Property $values |
-        # sort by name, then scope, then architecture:
-        Sort-Object -Property DisplayName, Scope, Architecture |
-        # add the property PSStandardMembers so PowerShell knows which properties to
-        # display by default:
-        Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
-    #endregion 
+        New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS -ErrorAction SilentlyContinue
+        $users = Get-ChildItem -Path "HKU:\"
+        
+        $list = @()
+        
+        foreach ($item in $users){
+            
+            $path1 = "HKU:\" +$item.Name+ "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+            $path2 = "HKU:\" +$item.Name+ "\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        
+            $list = $list + $path1 + $path2
+        }
+
+        $list = $list + 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*' + 
+                            'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' +
+                            'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*' +
+                            'HKCU:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+
+        $object = foreach($path in $list ) {
+ 
+            Get-ItemProperty -Path $path -ErrorAction Ignore |
+
+            Where-Object DisplayName |
+
+            Where-Object { $_.DisplayName -like $DisplayName } |
+
+            Select-Object -Property $values |
+
+
+            Add-Member -MemberType MemberSet -Name PSStandardMembers -Value $visibleProperties -PassThru
+
+        }
+
+        $object
+        $HKURemoved = $object.ForEach({ if($_.Name -ne "HKU"){$_} })
+        $HKURemoved
 }
-
 
 $software = Get-Software | Select-Object DisplayName, DisplayVersion
 
 $softcomputer = @{id = $env:COMPUTERNAME}
+
+Remove-Item $Object -ErrorAction Ignore
 $Object = New-Object PSObject -Property $softcomputer
 
 foreach ($soft in $software){
 
-    $Object | Add-Member -MemberType NoteProperty -Name $soft.DisplayName.replace(' ', '_').replace('"', '').replace("-","_").replace("(", "").replace(")", "").ToLower() -Value $soft.DisplayVersion
+    if ($soft.DisplayName -ne $null){
+        $Object | Add-Member -MemberType NoteProperty -Force -Name $soft.DisplayName.replace(' ', '_').replace('.', '_').replace('"', '').replace("-","_").replace("(", "").replace(")", "").ToLower() -Value $soft.DisplayVersion -ErrorAction Ignore
+    }
             
 }
 
-$list = ConvertTo-JSON $Object
-
+$doc = ConvertTo-JSON $Object
 
 ### Send information to elastic
 
@@ -148,4 +85,4 @@ $header = @{
     "Content-Type"="application/json"
 }
 
-Invoke-RestMethod -Uri $uri -Method Post -Body $list -Headers $header
+Invoke-RestMethod -Uri $uri -Method Post -Body $doc -Headers $header
